796fe158b001655dc0933a5c7eb98a3723fd61b4
IT26101915 <it26101915@my.sliit.lk>
2026-09-11 00:22:17 -0700

    Lab7Q2C.java

---
+++ IT26101915Lab7Q2C.java
@@ -0,0 +1,16 @@
+public class IT26101915Lab7Q2C {
+
+    public static void main(String[] args) {
+
+        for (int i = 5; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+    }
+}
+
